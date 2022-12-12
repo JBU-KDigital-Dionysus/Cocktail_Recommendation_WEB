b24e46cf7db29f3b926bad4e9ae3288ee31782b6
dnrc0714 <ziziang951@gmail.com>
2022-12-12 16:44:25 +0900

    프로젝트 생성

---
+++ src/main/java/com/cocktail/cocktail_recommendation/CocktailRecommendationApplication.java
@@ -0,0 +1,13 @@
+package com.cocktail.cocktail_recommendation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CocktailRecommendationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CocktailRecommendationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cocktail/cocktail_recommendation/CocktailRecommendationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cocktail.cocktail_recommendation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CocktailRecommendationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
