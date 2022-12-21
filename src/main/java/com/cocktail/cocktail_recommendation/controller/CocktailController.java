61f4402f8ef449a05c8d23f341066587c47bbd9f
dnrc0714 <ziziang951@gmail.com>
2022-12-21 15:23:50 +0900

    검색, 필터링

--- src/main/java/com/cocktail/cocktail_recommendation/controller/CocktailController.java
+++ src/main/java/com/cocktail/cocktail_recommendation/controller/CocktailController.java
@@ -4,6 +4,7 @@ package com.cocktail.cocktail_recommendation.controller;
 import com.cocktail.cocktail_recommendation.repository.CocktailRepository;
 
 import com.cocktail.cocktail_recommendation.dto.CocktailDto;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -11,11 +12,11 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.List;
 import java.util.Optional;
 
 @Controller
@@ -46,20 +47,39 @@ public class CocktailController {
         Page<CocktailDto> cocktailList = newCocktailRepository.findAll(pageable);
 
         model.addAttribute("cocktailList", cocktailList);
-        return "/cocktail/search";
+        return "/cocktail/cocktailList";
     }
 
     @GetMapping("/search.do")
     public String search(Model model,
                          @RequestParam(defaultValue = "0") int page,
-                         String ctName
+                         @RequestParam(required = false) String ctName,
+                         @RequestParam(required = false) String ctKindEng,
+                         HttpSession session
     ) {
+        session.removeAttribute("ctName");
+        session.removeAttribute("ctKindEng");
         final int ROWS = 20;
         Pageable pageable = PageRequest.of(page, ROWS, Sort.by("ctNo").ascending());
-        Page<CocktailDto> cocktailList = newCocktailRepository.findByCtNameContaining(ctName, pageable);
+        if (!StringUtils.hasText(ctName) && ctKindEng == null)  {
+            Page<CocktailDto> noCtNameCtKindEng = newCocktailRepository.findAll(pageable);
+            model.addAttribute("cocktailList", noCtNameCtKindEng);
+        }   else if (StringUtils.hasText(ctName) && ctKindEng == null)   {
+            Page<CocktailDto> noCtKindEng = newCocktailRepository.findByCtNameContaining(ctName, pageable);
+            session.setAttribute("ctName", ctName);
+            model.addAttribute("cocktailList", noCtKindEng);
+        }   else if (!StringUtils.hasText(ctName) && ctKindEng != null)  {
+            Page<CocktailDto> noCtName = newCocktailRepository.findByCtKindEng(ctKindEng, pageable);
+            session.setAttribute("ctKindEng", ctKindEng);
+            model.addAttribute("cocktailList", noCtName);
+        } else {
+            Page<CocktailDto> allYes = newCocktailRepository.findByCtNameContainingAndCtKindEng(ctName, ctKindEng, pageable);
+            session.setAttribute("ctName", ctName);
+            session.setAttribute("ctKindEng", ctKindEng);
+            model.addAttribute("cocktailList", allYes);
+        }
 
-        model.addAttribute("cocktailList", cocktailList);
-        return "/cocktail/search";
+        return "/search/cocktailSearch";
     }
 }
 
