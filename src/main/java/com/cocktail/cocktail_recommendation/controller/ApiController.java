56720c28a42b5c97be38ebfd9887b9f8f4ac1433
CorgiMuzi <kjhcorgi99@gmail.com>
2022-12-28 00:15:29 +0900

    ctAlcohol 안되는 문제 해결

--- src/main/java/com/cocktail/cocktail_recommendation/controller/ApiController.java
+++ src/main/java/com/cocktail/cocktail_recommendation/controller/ApiController.java
@@ -4,17 +4,16 @@ import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
-import java.util.Enumeration;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
+import java.util.*;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.http.HttpSession;
 import org.json.simple.JSONObject;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping(value = "/CONT")
@@ -28,33 +27,8 @@ public class ApiController {
 
     @Value("${debugMode}")
     public boolean debugMode;
-
-    // 	@PostMapping(value = "/upload")
-// 	@ResponseBody
-// 	public Map<String, String> imgUpload(HttpSession session) {
-//
-// 		Map<String, String> resultMap = new HashMap<String, String>();
-//
-// 		// 이미지 폴더 경로
-// 		String path = SAVE_PATH;
-//
-// 		JSONObject dbSrvJson = new JSONObject();
-// 		Enumeration<String> enumStr = session.getAttributeNames();
-// 		while(enumStr.hasMoreElements()){
-// 			String attrName = enumStr.nextElement();
-// 			dbSrvJson.put(attrName, session.getAttribute(attrName));
-// 		}
-// //		JSONObject result = getRecommendResult(pythonURL, "POST");
-// //		resultMap.put("result", result.toString());
-// 		System.out.println(dbSrvJson.toJSONString());
-// 		JSONObject result_json = getRecommendResult(pythonURL, dbSrvJson, "POST");
-//
-// 		resultMap.put("result", result_json.toJSONString());
-//
-// 		return resultMap;
-// 	}
     @GetMapping(value = "/upload")
-    public String imgUpload(HttpSession session) {
+    public String imgUpload(HttpSession session, RedirectAttributes redirectAttributes) {
         // 이미지 폴더 경로
         String path = SAVE_PATH;
 
@@ -67,56 +41,19 @@ public class ApiController {
 //		JSONObject result = getRecommendResult(pythonURL, "POST");
 //		resultMap.put("result", result.toString());
         JSONObject result_json = getRecommendResult(pythonURL, dbSrvJson, "POST");
-        return "redirect:/reco/results";
-    }
-
-    public JSONObject byPass(String url, JSONObject jsonData, String option) {
-        JSONObject responseJson = new JSONObject();
-        try {
-            // 연결할 url 생성
-            URL start_object = new URL(url);
-
-            // http 객체 생성
-            HttpURLConnection start_con = (HttpURLConnection) start_object.openConnection();
-            start_con.setDoOutput(true);
-            start_con.setDoInput(true);
-
-            // 설정 정보
-            start_con.setRequestProperty("Content-Type", "application/json");
-            start_con.setRequestProperty("Accept", "application/json");
-            start_con.setRequestMethod(option);
-
-            // 출력 부분
-
-            OutputStreamWriter wr = new OutputStreamWriter(start_con.getOutputStream());
-            wr.write(jsonData.toString());
-            wr.flush();
-
-            // 응답 받는 부분
-            StringBuilder start_sb = new StringBuilder();
-            int start_HttpResult = start_con.getResponseCode();
-
-            // 결과 성공일 경우 = HttpResult 200일 경우
-            if (start_HttpResult == HttpURLConnection.HTTP_OK) {
-                BufferedReader br = new BufferedReader(new InputStreamReader(start_con.getInputStream(), "utf-8"));
-                String line = null;
-                while ((line = br.readLine()) != null) {
-                    start_sb.append(line);
-                }
-                responseJson.put("data", start_sb);
-                responseJson.put("result", "SUCCESS");
-                br.close();
-                return responseJson;
-            } else {
-                // 그 외의 경우(실패)
-                responseJson.put("result", "FAIL");
-                return responseJson;
-            }
-        } catch (Exception e) {
+        ObjectMapper mapper = new ObjectMapper();
+        String json_str = result_json.toJSONString();
+        Integer recommendedCtId = 0;
+        try{
+            Map map = mapper.readValue(json_str, Map.class);
+            map = (Map)map.get("data");
+            recommendedCtId = (Integer)map.get("reco_0");
+        }catch (Exception e){
             e.printStackTrace();
-            responseJson.put("result", "EXCEPTION");
-            return responseJson;
         }
+
+        redirectAttributes.addAttribute("ctNo", recommendedCtId);
+        return "redirect:/cocktail/detail.do";
     }
 
     public JSONObject getRecommendResult(String pythonUrl, JSONObject pflavors, String requestMethod) {
